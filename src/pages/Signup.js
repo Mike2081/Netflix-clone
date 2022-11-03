7c41ee489918c3a84663de00432e0209430d81e9
Michael DoHo <michaeldoho@hotmail.com>
2022-11-02 21:58:32 -0400

    adding signIn page

---
+++ src/pages/Signup.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <>
+        <div className='w-full h-screen'>
+            <img className='hidden sm:block absolute w-full h-full object-cover' src='https://assets.nflxext.com/ffe/siteui/vlv3/79fe83d4-7ef6-4181-9439-46db72599559/d2762f15-5a76-43cc-aff4-91fc771bb69d/CA-en-20221017-popsignuptwoweeks-perspective_alpha_website_medium.jpg' alt='/' />
+            <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
+        </div>
+    </>
+  )
+}
+
+export default Signup
